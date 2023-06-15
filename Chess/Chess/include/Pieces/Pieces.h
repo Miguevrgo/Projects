c63ddd052179ea08e122fca834324da9a37b95d1
Miguel <miguevrgo@gmail.com>
2023-06-15 12:51:03 +0200

    Update Chess structure

--- Chess/Chess/include/Pieces/Pieces.h
+++ Chess/Chess/include/Pieces/Pieces.h
@@ -1,40 +1,25 @@
 #pragma once
 #include <string>
+#include "Player.h"
 
 struct Coordinates {
 	unsigned int row;
-	char col;
+	unsigned int col;
 };
-
+// Name of class should be changed to Piece and reorganize all
 class Pieces{
 public:
-	Pieces(const std::string colour);
-	~Pieces();
-	Coordinates getCoordinates();
-	void setCoordinates(Coordinates coords);
-	/**
-	 * @brief Checks if given move is valid. Checks if king is or would be in 
-	 * check and if the move is contained in piece valid moves
-	 * 
-	 * @return true if the given move is valid
-	 * @return false if the given move cannot be performed
-	 */
-	bool isValidMovement(Coordinates move); // Deciding between isValidMovement or getValidMovements
-	/**
-	 * @brief Returns if the given movement is a check
-	 * 
-	 * @return true 
-	 * @return false 
-	 */
-	bool isCheck(Coordinates move);
-	/**
-	 * @brief Changes the current piece coronated to the provided one 
-	 * (Queen,Rook,Knight or Bishop)
-	 * 
-	 * @param new_piece 
-	 */
-	void changePiece(Pieces new_piece);
-private: 
-	const std::string colour;
+	bool initPiece(int id, Player owner, Coordinates pos);
+	Pieces* getPiece(Coordinates pos);
+	virtual bool makeMove(Coordinates start, Coordinates end);
+	virtual void run();
+	virtual bool gameOver() const = 0;
+protected:
+	static unsigned const int SIZE = 8;
+	unsigned int turn;
+	Pieces* matrixPieces[SIZE][SIZE];
+	Pieces* newPiece(int id, Player owner);
+private:
+	void initializeMatrix();
 };
 
