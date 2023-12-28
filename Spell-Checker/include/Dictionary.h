96b90cbc2fd20452e814f92f7997fd9804f0d2a5
Miguevrgo <miguevrgo@gmail.com>
2023-12-28 23:36:31 +0100

    Definition for classes Trie.h and Dictionary.h

--- Spell-Checker/include/Dictionary.h
+++ Spell-Checker/include/Dictionary.h
@@ -7,9 +7,22 @@
 #ifndef SPELL_CHECKER_DICTIONARY_H
 #define SPELL_CHECKER_DICTIONARY_H
 
+#include "Trie.h"
 
 class Dictionary {
+private:
+    Trie trie; // Structure that holds the dictionary words
+public:
+    Dictionary();
+    Dictionary(const std::string &filename);
+    ~Dictionary();
 
+    bool LoadFromFile(const std::string &filename);
+    bool AddWord(const std::string &word);
+    bool RemoveWord(const std::string &word);
+    bool CheckWord(const std::string &word);
+    std::vector<std::string> SuggestCorrections(const std::string &word, int maxDistance) const;
+    bool SaveToFile(const std::string &filename) const;
 };
 
 
