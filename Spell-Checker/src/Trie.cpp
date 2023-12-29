c163893c9d9bd0851e5c6c053310b4e4f150472d
Miguevrgo <miguevrgo@gmail.com>
2023-12-29 18:44:23 +0100

    Implement methods Search, Remove and StartsWith

--- Spell-Checker/src/Trie.cpp
+++ Spell-Checker/src/Trie.cpp
@@ -30,3 +30,54 @@ void Trie::Insert(const std::string &word) {
     }
     node->IsEndOfWord = true;
 }
+
+bool Trie::Remove(const std::string &word) {
+    return removeHelper(root, word, 0);
+}
+
+bool Trie::removeHelper(TrieNode *node, const std::string &word, int depth) {
+    if (!node){
+        return false;
+    }
+    if (depth == word.size()) {
+        if (node->IsEndOfWord) {
+            node->IsEndOfWord = false;
+            return node->children.empty();
+        }
+
+        return false;
+    }
+
+    char c = word[depth];
+    TrieNode* child = node->children[c];
+    bool ShouldDeleteChild = removeHelper(child, word, depth + 1);
+
+    if (ShouldDeleteChild) {
+        node->children.erase(c);
+        return node->children.empty();
+    }
+
+    return false;
+}
+
+bool Trie::Search(const std::string &word) const {
+    const TrieNode* node = root;
+    for (auto& c : word){
+        if (!node->children.count(c)){
+            return false;
+        }
+        node = node->children.at(c);
+    }
+    return (node && node->IsEndOfWord);
+}
+
+bool Trie::StartsWith(const std::string &prefix) const {
+    const TrieNode* node = root;
+    for (auto& c : prefix){
+        if (!node->children.contains(c)) {
+            return false;
+        }
+        node = node->children.at(c);
+    }
+    return true;
+}
