72e8ff7e89be1403442f45b9cf864a828767d360
Miguevrgo <miguevrgo@gmail.com>
2024-01-05 15:13:29 +0100

    Provide documentation for corrector class

--- Spell-Checker/include/Corrector.h
+++ Spell-Checker/include/Corrector.h
@@ -15,13 +15,41 @@
 class Corrector {
 private:
     const Dictionary& dictionary;
+
+/**
+     * @brief Calculates the Levenshtein distance between two strings.
+     * @param s1 First string.
+     * @param s2 Second string.
+     * @return The Levenshtein distance between s1 and s2.
+     */
     int CalculateLevenshteinDistance(const std::string& s1, const std::string& s2);
+
+    /**
+     * @brief Calculates the distance between two strings using the keyboard layout.
+     * @param s1 First string.
+     * @param s2 Second string.
+     * @return The distance between s1 and s2 in an QWERTY keyboard.
+     */
     double GetDistanceFromKeyboard(const std::string& s1, const std::string& s2);
 public:
     Corrector() = delete;
     ~Corrector() = default;
     explicit Corrector(const Dictionary& dictionary) : dictionary(dictionary) {};
+
+    /**
+     * @brief Suggests all the possible corrections of a word where the Levenshtein distance is less than 2.
+     * @param word the word to be corrected.
+     * @return A multimap with the possible corrections and their distance to the original word.
+     */
     std::multimap<double, std::string> SuggestCorrections(const std::string& word);
+
+    /**
+     * @brief Gets the top N suggestions from a multimap of corrections, it orders the corrections by distance.
+     * if two corrections have the same distance, it orders them by frequency.
+     * @param corrections The multimap of corrections.
+     * @param topN The number of suggestions to be returned.
+     * @return A vector with the top N suggestions.
+     */
     std::vector<std::string> GetTopSuggestions(const std::multimap<double, std::string>& corrections, int topN);
 };
 
