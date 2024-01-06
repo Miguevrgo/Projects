8beab5855576b6f2a4849ca5e765e67da8961086
Miguevrgo <miguevrgo@gmail.com>
2024-01-06 23:07:16 +0100

    Provide documentation for Dictionary.h

--- Spell-Checker/include/Dictionary.h
+++ Spell-Checker/include/Dictionary.h
@@ -18,11 +18,36 @@ public:
     ~Dictionary() = default;
     Dictionary(const std::string &filename, const std::string &language);
     void SetLanguage(const std::string &newLang);
+
+    /**
+     * @brief Loads the words from a file into the dictionary. As well as the frequency of each word.
+     * @param filename
+     * @return true if the file was loaded successfully, false otherwise.
+     */
     bool LoadFromFile(const std::string &filename);
+
+    /**
+     * @brief Adds a word to the dictionary.
+     * @param word The word to be added.
+     * @param frequency The frequency of the word.
+     */
     void AddWord(const std::string &word, unsigned int frequency);
+
+    /**
+     * @brief Removes a word from the dictionary.
+     * @param word The word to be removed.
+     * @return true if the word was removed successfully, false otherwise.
+     */
     bool RemoveWord(const std::string &word);
     bool CheckWord(const std::string &word);
     int GetFrequency(const std::string &word) const;
+
+    /**
+     * @brief Gets all the words in a given range of length.
+     * @param minLength of the words.
+     * @param maxLength of the words.
+     * @return A vector with all the words in the given range.
+     */
     [[nodiscard]] std::vector<std::string> GetWordsOfLengthRange(int minLength, int maxLength) const;
     Dictionary &operator=(const Dictionary &rhs);
     Dictionary operator+(const std::string &word);
