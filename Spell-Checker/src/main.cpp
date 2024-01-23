0905339f70ee73a008f88a7d094541344f0c1284
miguevr <miguevrgo@gmail.com>
2024-01-23 14:46:02 +0100

    Use wxWidgets to implement a GUI

--- Spell-Checker/src/main.cpp
+++ Spell-Checker/src/main.cpp
@@ -1,37 +1,72 @@
-#include <iostream>
+#include <wx/wx.h>
+#include <sstream>
 #include "Corrector.h"
+#include "Dictionary.h"
 
-int main(int argc, char* argv[]) {
+class SpellCheckerFrame : public wxFrame {
+public:
+    SpellCheckerFrame(const wxString& title)
+            : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(500, 300)) {
 
-    std::string resourcedir = "./resources";
+        wxPanel *panel = new wxPanel(this, -1);
+        wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
 
-    // Loading word from input
-    int option;
-    do {
-        std::cout << "Choose a language: " << std::endl;
-        std::cout << "\t [1] English" << std::endl;
-        std::cout << "\t [2] Spanish" << std::endl;
-        std::cout << "Option: ";
-        std::cin >> option;
-    } while (option != 1 && option != 2);
+        wxStaticText *st1 = new wxStaticText(panel, wxID_ANY, wxT("Elige un idioma:"));
+        vbox->Add(st1, 0, wxLEFT | wxTOP, 10);
 
-    std::string language = (option == 1) ? "english.txt" : "spanish.txt";
+        wxArrayString choices;
+        choices.Add(wxT("English"));
+        choices.Add(wxT("Spanish"));
+        wxChoice *choice = new wxChoice(panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, choices);
+        vbox->Add(choice, 0, wxLEFT | wxTOP, 10);
 
-    Dictionary dictionary(resourcedir + "/" + language, language);
-    Corrector corrector(dictionary);
+        wxStaticText *st2 = new wxStaticText(panel, wxID_ANY, wxT("Ingresa una palabra o frase:"));
+        vbox->Add(st2, 0, wxLEFT | wxTOP, 10);
 
-    std::string word;
-    std::cout << "Enter a word or a phrase, or type 'exit' to exit: ";
-    do{
-        std::cin >> word;
+        wxTextCtrl *tc = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+        vbox->Add(tc, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
 
-        if (word == "exit"){break;}
+        wxButton *btn = new wxButton(panel, wxID_ANY, wxT("Corregir"));
+        vbox->Add(btn, 0, wxALIGN_RIGHT | wxRIGHT | wxBOTTOM, 10);
 
-        std::vector<std::string> suggestions = corrector.GetTopSuggestions(corrector.SuggestCorrections(word), 1);
-        for (const auto& suggestion : suggestions){
-            std::cout << suggestion << std::endl;
-        }
-    } while (word != "exit");
+        panel->SetSizer(vbox);
+        Centre();
 
-    return 0;
-}
+        btn->Bind(wxEVT_BUTTON, [this, tc, choice](wxCommandEvent& event) {
+            std::string resourcedir = "./resources";
+            std::string language = (choice->GetSelection() == 0) ? "english.txt" : "spanish.txt";
+
+            Dictionary dictionary(resourcedir + "/" + language, language);
+            Corrector corrector(dictionary);
+
+            std::string inputText = tc->GetValue().ToStdString();
+            std::istringstream iss(inputText);
+            std::string word;
+            wxString result;
+            wxTextAttr redAttr(*wxRED);
+            wxTextAttr greenAttr(*wxGREEN);
+
+            tc->Clear();
+
+            while (iss >> word) {
+                std::vector<std::string> suggestions = corrector.GetTopSuggestions(corrector.SuggestCorrections(word), 1);
+                wxString correctedWord = (!suggestions.empty()) ? wxString(suggestions[0]) : wxString(word);
+
+                wxTextAttr colorAttr = (correctedWord.IsSameAs(word, false)) ? greenAttr : redAttr;
+                tc->SetDefaultStyle(colorAttr);
+                tc->AppendText(correctedWord + " ");
+            }
+        });
+    }
+};
+
+class SpellCheckerApp : public wxApp {
+public:
+    virtual bool OnInit() {
+        SpellCheckerFrame *frame = new SpellCheckerFrame(wxT("Corrector Ortográfico"));
+        frame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(SpellCheckerApp);
