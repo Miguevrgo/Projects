efea2de0de6cea8281f7563bd710f4825b05df32
miguevr <miguevrgo@gmail.com>
2024-02-03 10:03:51 +0100

    Define some Algorithms

--- Graph-Algo-Visual/include/Algorithms.h
+++ Graph-Algo-Visual/include/Algorithms.h
@@ -7,10 +7,30 @@
 #ifndef GRAPH_ALGO_VISUAL_ALGORITHMS_H
 #define GRAPH_ALGO_VISUAL_ALGORITHMS_H
 
+#include <vector>
+#include "Graph.h"
 
 class Algorithms {
+public:
+    // Depth-First Search
+    static std::vector<int> DFS( int startVertex);
+
+    // Breadth-First Search
+    static std::vector<int> BFS(int startVertex);
+
+    // Dijkstra's Algorithm for Shortest Paths
+    static std::vector<int> Dijkstra(int startVertex);
+
+    // Kruskal's Algorithm for Minimum Spanning Tree
+    static std::vector<Edge> Kruskal();
+
+    // Prim's Algorithm
+    static std::vector<Edge> Prim();
+
+private:
 
 };
+
 
 
 #endif //GRAPH_ALGO_VISUAL_ALGORITHMS_H
