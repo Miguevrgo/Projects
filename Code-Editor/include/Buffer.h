90402792931fc424d031e202f011bc9a064224df
miguevr <miguevrgo@gmail.com>
2024-02-06 19:53:09 +0100

    New Project February

---
+++ Code-Editor/include/Buffer.h
@@ -0,0 +1,14 @@
+//
+// Created by miguevr on 2/6/24.
+//
+
+#ifndef CODE_EDITOR_BUFFER_H
+#define CODE_EDITOR_BUFFER_H
+
+
+class Buffer {
+
+};
+
+
+#endif //CODE_EDITOR_BUFFER_H
---
+++ Code-Editor/src/Buffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by miguevr on 2/6/24.
+//
+
+#include "../include/Buffer.h"
