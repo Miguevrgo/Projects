7059a16d12cbb924d242041d8b9116017423ab2a
Miguevrgo <miguevrgo@gmail.com>
2024-02-08 16:18:25 +0100

    First main approach

--- Code-Editor/main.cpp
+++ Code-Editor/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char input;
+    while(true) {
+        std::cin >> input;
+        if (input != ':'){
+            std::cout << input;
+        }
+        else{
+            std::cin >> input;
+            switch(input){
+                case 'q':
+                    break;
+                case 's':
+                    // Save current terminal content to file with name by buffer
+                    break;
+            }
+        }
+    }
     return 0;
 }
