49911ffc16baf225d22914371ad4f06e4b208dc0
miguevr <miguevrgo@gmail.com>
2024-02-08 20:31:59 +0100

    Definition and methods for cursor class

---
+++ Code-Editor/Cursor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by miguevr on 2/8/24.
+//
+
+#include "Cursor.h"
---
+++ Code-Editor/Cursor.h
@@ -0,0 +1,35 @@
+/**
+ * @file Cursor.h
+ * @author Miguel Ángel De la Vega Rodríguez
+ * @brief This file contains the declaration of the Cursor class used to
+ * represent the position of the cursor in the text editor.
+ */
+
+#ifndef CODE_EDITOR_CURSOR_H
+#define CODE_EDITOR_CURSOR_H
+
+
+class Cursor {
+public:
+    Cursor();
+    Cursor(int lineNumber, int charNumber);
+
+    void MoveUp(int numLines = 1);
+    void MoveDown(int numLines = 1);
+    void MoveRight(int numChars = 1);
+    void MoveLeft(int numChars = 1);
+
+    void MoveEnd();
+    void MoveStart();
+
+
+
+private:
+    int lineNumber;
+    int charNumber;
+
+    void UpdatePos(int verticalPos, int horizontalPos);
+};
+
+
+#endif //CODE_EDITOR_CURSOR_H
