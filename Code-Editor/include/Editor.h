5b6a08876c623788253028e154e06da2e035ddf5
miguevr <miguevrgo@gmail.com>
2024-02-12 09:58:58 +0100

    Class Editor

--- Code-Editor/include/Editor.h
+++ Code-Editor/include/Editor.h
@@ -1,13 +1,26 @@
-//
-// Created by miguevr on 2/6/24.
-//
+/**
+ * @file Editor.h
+ * @author Miguel Ángel De la Vega Rodríguez
+ * @brief Editor class to implement useful functions
+ */
 
 #ifndef CODE_EDITOR_EDITOR_H
 #define CODE_EDITOR_EDITOR_H
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
 
 class Editor {
+public:
+    bool SaveFile(const std::string& fileName);
+    bool HasChanged();
+    int GetCharContained(int startLine, int startChar, int endLine, int endChar);
 
+    void SwapLines(int lineA, int LineB);
+private:
+    bool documentChanged;
 };
 
 
--- Code-Editor/src/Editor.cpp
+++ Code-Editor/src/Editor.cpp
@@ -1,5 +1,7 @@
-//
-// Created by miguevr on 2/6/24.
-//
+/**
+ * @file Editor.cpp
+ * @author Miguel Ángel De la Vega Rodríguez
+ * @brief Implementation for Editor class
+ */
 
 #include "Editor.h"
