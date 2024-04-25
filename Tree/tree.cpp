848c8a249c021e95f0cfcdf73965ca6b6c270fb1
Miguevrgo <Miguevrgo@gmail.com>
2024-04-25 13:15:27 +0200

    Upload definition for insert

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -2,6 +2,42 @@
 
 template <typename T>
 inline void Tree<T>::insert(const T &node) {
-    //TODO:
-    ;
+    if (!root) {
+        root = node;
+    }
+    else {
+        Node *current = root.get();
+        while (current) {
+            if (node < current->data) {
+                if (!current->leftChild) {
+                    current->leftChild = std::make_unique<Node>(node);
+                    return;
+                }
+                current = current->leftChild.get();
+            }
+            else {
+                if (!current->rightChild) {
+                    current->rightChild = std::make_unique<Node>(node);
+                    return;
+                }
+                current = current->rightChild.get();
+            }
+        }
+    }
+    
+}
+
+template <typename T>
+constexpr bool Tree<T>::empty() const noexcept {
+    return root;
+}
+
+template <typename T>
+T Tree<T>::leftChild(const Node &node) const noexcept {
+    return node->leftChild->data;
+}
+
+template <typename T>
+T Tree<T>::rightChild(const Node &node) const noexcept {
+    return node->rightChild->data;
 }
