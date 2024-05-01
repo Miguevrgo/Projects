0d381480b143f3f5c98ddfc6f0a24d2b31d9e3bf
miguevrgo <miguevrgo@gmail.com>
2024-05-01 22:37:47 +0200

    Update

--- EzGit/src/GitManager.cpp
+++ EzGit/src/GitManager.cpp
@@ -4,7 +4,7 @@
 
 #include "GitManager.h"
 
-std::vector<std::string> GitManager::listRepositories() {
+std::vector<std::string> GitManager::listRepositories() const {
     std::vector<std::string> repositories;
     try {
         for (const auto& entry : std::filesystem::directory_iterator(baseDirectory)) {
@@ -19,5 +19,38 @@ std::vector<std::string> GitManager::listRepositories() {
     catch(const std::filesystem::filesystem_error& err){
         std::cerr << "Error accesing directory: " << err.what() << "\n Revise privileges in base Folder" << std::endl;
     }
+
     return repositories;
+}
+
+std::string GitManager::gitShortcut(const std::string& shortcut) const {
+    // Define the shortcuts for the commands //GIT LOG IMPORTANT BUT NOT HERE
+    std::unordered_map<std::string, std::string> shortcuts = {
+            {"gs", "status"},
+            {"gk", "push"},
+            {"gj", "pull"}
+            //{"fetch", "fetch"},
+    };
+
+    return shortcuts[shortcut];
+
+}
+
+void GitManager::gitCommand(const std::string &command, const std::string &repository) const {
+    std::string cmd = "cd " + baseDirectory + "/" + repository + "&& git" + command;
+    std::system(cmd.c_str());
+}
+
+void GitManager::executeCommand(const std::string &command, const std::string& repository,
+        const std::string commit) const 
+{
+    std::string toExecute = gitShortcut(command);
+    if (commit == "NOT_PUSH") {
+        gitCommand(toExecute, repository);
+    }
+    else{
+        gitCommand("add .", repository);
+        gitCommand("commit -m \"" + commit + "\"", repository);
+        gitCommand(toExecute,repository);
+    }
 }
