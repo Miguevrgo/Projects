ed4df6523252d68dd3841f63219d72356b3fffbf
miguevrgo <miguevrgo@gmail.com>
2024-05-20 20:41:50 +0200

    Ridmaze GUI with sprites

--- Ridmaze/include/GameController.h
+++ Ridmaze/include/GameController.h
@@ -1,15 +1,11 @@
-//
-// Created by miguevr on 5/17/24.
-//
-
 #ifndef RIDMAZE_GAMECONTROLLER_H
 #define RIDMAZE_GAMECONTROLLER_H
 
-
-#include "Game.h"
-#include "TextUI.h"
 #include <SFML/Graphics.hpp>
 #include <memory>
+#include <string>
+#include "TextUI.h"
+#include "Game.h"
 
 class GameController {
 public:
@@ -17,17 +13,34 @@ public:
     void run();
 
 private:
-    Game game;
-    sf::RenderWindow window;
-    sf::Font font;
-    std::unique_ptr<TextUI> ui;
-
     void processEvents();
     void update();
     void render();
     void handlePlayerInput(sf::Keyboard::Key key);
     void drawGameState(const GameState& state);
+    void showMainMenu();
+    void gameLoop();
+
+    Game game;
+    sf::RenderWindow window;
+    sf::Font font;
+
+    sf::Texture menuTexture;
+    sf::Sprite menuSprite;
+
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+    sf::Texture monsterTexture;
+    sf::Sprite monsterSprite;
+    sf::Texture blockTexture;
+    sf::Sprite blockSprite;
+    sf::Texture emptyTexture;
+    sf::Sprite emptySprite;
+    sf::Texture exitTexture;
+    sf::Sprite exitSprite;
+
+    std::unique_ptr<TextUI> ui;
 };
 
 
-#endif //RIDMAZE_GAMECONTROLLER_H
+#endif // RIDMAZE_GAMECONTROLLER_H
