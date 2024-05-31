2982161bde0f3965023414a3b2cff4387ddfdf73
Miguevrgo <Miguevrgo@gmail.com>
2024-05-31 15:47:07 +0200

    Upload MoveUp and MoveDown implementation

--- Ridmaze/src/Menu.cpp
+++ Ridmaze/src/Menu.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Menu.h"
+
+void Menu::moveUp() {
+    if (selectedIndex > 0) {
+        --selectedIndex;
+        if (state == MAIN_MENU) {
+            mainMenuOptions[selectedIndex].setFillColor(sf::Color::Red);
+            mainMenuOptions[selectedIndex + 1].setFillColor(sf::Color::White);
+        }
+        else {
+            settingsOptions[selectedIndex].setFillColor(sf::Color::Red);
+            settingsOptions[selectedIndex + 1].setFillColor(sf::Color::White);
+        }
+    }
+}
+
+void Menu::moveDown() {
+    if (state == MAIN_MENU && selectedIndex + 1 < MAIN_MENU_OPTIONS) {
+        mainMenuOptions[selectedIndex].setFillColor(sf::Color::White);
+        ++selectedIndex;
+        mainMenuOptions[selectedIndex].setFillColor(sf::Color::Red);
+    }
+    else if (state == SETTINGS_OPTIONS && selectedIndex + 1 < MAIN_MENU_OPTIONS){
+        settingsOptions[selectedIndex].setFillColor(sf::Color::White);
+        ++selectedIndex;
+        settingsOptions[selectedIndex].setFillColor(sf::Color::Red);
+    }
+}
