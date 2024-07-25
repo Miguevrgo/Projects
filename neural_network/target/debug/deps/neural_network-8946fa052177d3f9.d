8cb2f784b74da9e8c08135429eda03d0130763b1
Miguevr <miguevrgo@gmail.com>
2024-07-25 15:32:34 +0200

    Upload Project Algori [gt4-rs Application to visualize algorithms]

--- neural_network/target/debug/deps/neural_network-8946fa052177d3f9.d
+++
@@ -1,5 +0,0 @@
-/home/miguevr/GitHub/Projects/neural_network/target/debug/deps/libneural_network-8946fa052177d3f9.rmeta: src/main.rs
-
-/home/miguevr/GitHub/Projects/neural_network/target/debug/deps/neural_network-8946fa052177d3f9.d: src/main.rs
-
-src/main.rs:
--- neural_network/target/debug/neural_network.d
+++
@@ -1,1 +0,0 @@
-/home/miguevr/GitHub/Projects/neural_network/target/debug/neural_network: /home/miguevr/GitHub/Projects/neural_network/src/main.rs
