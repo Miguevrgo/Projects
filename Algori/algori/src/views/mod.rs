bfdaa490538075e718eeef547067b0743210605c
Miguevr <miguevrgo@gmail.com>
2024-07-29 18:03:26 +0200

    Upload files for each algorithm view, document create_home_view

--- Algori/algori/src/views/mod.rs
+++ Algori/algori/src/views/mod.rs
@@ -1,1 +1,14 @@
 pub mod array;
+pub mod binary_search;
+pub mod bit_manipulation;
+pub mod dijkstra;
+pub mod graph;
+pub mod hash_table;
+pub mod heap;
+pub mod linked_list;
+pub mod math;
+pub mod queue;
+pub mod sorting;
+pub mod stack;
+pub mod tree;
+pub mod trie;
