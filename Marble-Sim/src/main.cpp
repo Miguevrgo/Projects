7960bf09542104936d1f8eb10310ccdc197b2c0d
Miguevr <miguevrgo@gmail.com>
2024-08-10 16:34:21 +0200

    Upload math class for Transitions

--- Marble-Sim/src/main.cpp
+++ Marble-Sim/src/main.cpp
@@ -6,6 +6,9 @@
 #include "stb_image.h"
 
 #include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 const unsigned int SCR_WIDTH = 1366;
