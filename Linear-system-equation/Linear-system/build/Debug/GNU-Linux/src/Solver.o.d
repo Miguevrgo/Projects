c808dfc3ec9e6f4bed17d45d4eae6362ea473a29
Miguevr <miguevrgo@gmail.com>
2024-08-14 18:30:35 +0200

    Update Projects and README.md

--- Linear-system-equation/Linear-system/build/Debug/GNU-Linux/src/Solver.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/GNU-Linux/src/Solver.o: src/Solver.cpp include/Solver.h
-include/Solver.h:
--- Linear-system-equation/Linear-system/build/Debug/GNU-Linux/src/main.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/GNU-Linux/src/main.o: src/main.cpp include/Solver.h
-include/Solver.h:
