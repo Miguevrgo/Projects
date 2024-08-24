83c26a93218e83a65fd1e2dfb8714b96c6a94acc
Miguevr <miguevrgo@gmail.com>
2024-08-24 10:59:19 +0200

    Upload up movement

--- Oxide-Editor/src/editor.rs
+++ Oxide-Editor/src/editor.rs
@@ -78,17 +78,20 @@ impl Editor {
             if let Some(key) = Terminal::read_key() {
                 match key {
                     KeyCode::Char('i') => break,
+                    KeyCode::Char('q') => {
+                        self.exit();
+                        std::process::exit(0);
+                    }
                     KeyCode::Char('h') => self.move_cursor_left(),
                     KeyCode::Char('l') => self.move_cursor_right(),
                     KeyCode::Char('j') => {
                         if self.cursor_y < self.buffer.get_num_lines() - 1 {
-                            //self.buffer.cursor_down();
                             self.cursor_y += 1;
                         }
                     }
                     KeyCode::Char('k') => {
                         if self.cursor_y > 0 {
-                            //self.buffer.cursor_up();
+                            self.cursor_x = self.buffer.cursor_up(self.cursor_x);
                             self.cursor_y -= 1;
                         }
                     }
@@ -100,7 +103,7 @@ impl Editor {
 
                     KeyCode::Char('0') => {
                         self.cursor_x = 0;
-                        //self.buffer.cursor_to_line_start();
+                        self.buffer.cursor_to_line_start();
                     }
                     KeyCode::Char('$') => {
                         self.cursor_x =
