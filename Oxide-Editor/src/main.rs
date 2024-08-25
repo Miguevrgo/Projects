18867ce9f1ad329cdcbfa27d8828e5039ea22297
Paula Lopez <paulalopez@MacBook-Air-de-Paula.local>
2024-08-25 14:08:35 +0200

    Improve get_lines, refactor main

--- Oxide-Editor/src/main.rs
+++ Oxide-Editor/src/main.rs
@@ -4,20 +4,35 @@ mod status_bar;
 mod terminal;
 
 use editor::Editor;
+use std::env;
+use std::fs;
+use std::io;
+use std::path::Path;
 
-fn main() -> Result<(), std::io::Error> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() == 2 {
-        let filename = &args[1];
-        let file_path = std::path::Path::new(filename);
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
 
-        if !std::path::Path::is_file(file_path) {
-            std::fs::File::create(filename)?;
-        }
-        let mut editor = Editor::new(filename);
-        editor.run();
-        editor.exit();
+    if args.len() > 2 {
+        eprintln!("Usage: {} [filename]", args[0]);
+        std::process::exit(1);
     }
+
+    let filename = if args.len() == 2 {
+        args[1].clone()
+    } else {
+        String::from("untitled.txt")
+    };
+
+    let file_path = Path::new(&filename);
+
+    if !file_path.exists() {
+        fs::File::create(&filename)?;
+    }
+
+    let mut editor = Editor::new(&filename);
+    editor.run();
+    editor.exit();
+    std::fs::remove_file(file_path)?;
 
     Ok(())
 }
