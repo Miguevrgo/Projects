a488afb64560f75eae7ee50207fa4f8a008780b0
Miguevr <miguevrgo@gmail.com>
2024-09-07 23:08:57 +0200

    Just adding home to all views with style applied

--- Algori/algori/src/views/binary_search.rs
+++ Algori/algori/src/views/binary_search.rs
@@ -5,6 +5,7 @@ pub fn create_view(stack: &gtk::Stack) -> Box {
     let label = Label::new(Some("This is the view for Array"));
     let stack_clone = stack.clone();
     let home_button = Button::with_label("Home");
+    home_button.set_widget_name("home-button");
 
     home_button.connect_clicked(move |_| {
         stack_clone.set_visible_child_name("Home");
