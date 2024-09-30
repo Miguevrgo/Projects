c84d693e76faea2cdd9d6416d4edad3214d074ec
miguevrgo <miguevrgo@gmail.com>
2024-09-30 13:07:17 +0200

    Upload simple terminal database

--- Oxide-Database/src/database.rs
+++ Oxide-Database/src/database.rs
@@ -1,5 +1,7 @@
+use bincode::Error;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::fmt;
 use std::fs::File;
 use std::io::{Read, Write};
 
@@ -9,6 +11,16 @@ pub struct Person {
     pub surname: String,
     pub dni: String,
     pub age: u32,
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "Name: {}\n Surname: {}\n DNI: \n {}, Age: {}",
+            self.name, self.surname, self.dni, self.age
+        )
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,12 +41,26 @@ impl Database {
         self.data.insert(person.dni.clone(), person);
     }
 
+    pub fn delete(&mut self, dni: &str) {
+        self.data.remove_entry(dni);
+    }
+
     pub fn get(&self, dni: &str) -> Option<&Person> {
         self.data.get(dni)
     }
 
     pub fn save(&self) -> std::io::Result<()> {
         let encoded: Vec<u8> = bincode::serialize(&self).expect("Error serializing");
+        let mut file = File::create(&self.file_path)?;
+        file.write_all(&encoded)?;
         Ok(())
+    }
+
+    pub fn load(file_path: &str) -> std::io::Result<Database> {
+        let mut file = File::open(file_path)?;
+        let mut encoded = Vec::new();
+        file.read_to_end(&mut encoded)?;
+        let db: Database = bincode::deserialize(&encoded).expect("Error deserializing");
+        Ok(db)
     }
 }
--- Oxide-Database/src/main.rs
+++ Oxide-Database/src/main.rs
@@ -1,31 +1,75 @@
-use std::fs::{remove_file, OpenOptions};
-use std::io::{BufWriter, Write};
-use std::path::{Path, PathBuf};
+use std::io::{self, Write};
 
 mod database;
 
-fn save_data(path: &Path, data: &[u8]) -> std::io::Result<()> {
-    let temp_path = PathBuf::from(format!("{}.temp", path.display()));
-    let temp_file = OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(&temp_path)?;
-
-    let mut writer = BufWriter::new(temp_file);
-    writer.write_all(data)?;
-    writer.flush()?;
-    writer.get_ref().sync_data()?;
-
-    std::fs::rename(&temp_path, path)?;
-    if temp_path.exists() {
-        remove_file(&temp_path)?;
-    }
-
-    Ok(())
-}
+use crate::database::{Database, Person};
 
 fn main() {
-    let file_path = Path::new("test");
-    let content = b"hello world";
-    save_data(file_path, content).unwrap();
+    let mut db: Database = Database::load("data").unwrap();
+
+    print!("\x1B[2J\x1B[1;1H");
+    println!(
+        "╔════════════════════════════╗\n║  Welcome to Oxide Database ║\n╚════════════════════════════╝"
+    );
+    loop {
+        println!("[1] ~ Insert Person");
+        println!("[2] ~ Delete Person");
+        println!("[3] ~ Find Person");
+        println!("[4] ~ Exit");
+
+        let choice = read_input();
+
+        match choice.as_str() {
+            "1" => insert_person(&mut db),
+            "2" => delete_person(&mut db),
+            "3" => find_person(&mut db),
+            _ => break,
+        };
+    }
+
+    db.save().unwrap();
+}
+
+fn read_input() -> String {
+    let mut option = String::new();
+    std::io::stdin().read_line(&mut option).unwrap();
+    option.trim().to_string()
+}
+
+fn insert_person(database: &mut Database) {
+    print!("DNI: ");
+    io::stdout().flush().unwrap();
+    let dni = read_input();
+    print!("Name: ");
+    io::stdout().flush().unwrap();
+    let name = read_input();
+    print!("Surname: ");
+    io::stdout().flush().unwrap();
+    let surname = read_input();
+    print!("Age: ");
+    io::stdout().flush().unwrap();
+    let age: u32 = read_input().parse().expect("Invalid age");
+
+    database.insert(Person {
+        dni,
+        name,
+        surname,
+        age,
+    });
+}
+
+fn delete_person(database: &mut Database) {
+    println!("Insert DNI to delete:");
+    let dni = read_input();
+    database.delete(&dni);
+}
+
+fn find_person(database: &mut Database) {
+    println!("DNI: ");
+    let dni = read_input();
+    if let Some(person) = database.get(&dni) {
+        println!("{person}");
+    } else {
+        println!("Person with DNI: {} Not found", dni);
+    }
 }
