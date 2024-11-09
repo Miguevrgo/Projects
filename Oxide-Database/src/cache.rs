d93cc6d40fa9f3147b2732c2f99eb4d9a02d6367
Miguevrgo <miguevrgo@gmail.com>
2024-11-09 22:39:03 +0100

    1

--- Oxide-Database/src/cache.rs
+++ Oxide-Database/src/cache.rs
@@ -1,7 +1,25 @@
-pub struct Cache {}
+use crate::table::Page;
+const PAGES_IN_CACHE: usize = 5;
+
+pub struct Cache {
+    pages: [(Option<usize>, Option<Page>); PAGES_IN_CACHE],
+}
 
 impl Cache {
     pub fn new() -> Self {
-        Cache {}
+        Cache {
+            pages: [(None, None); PAGES_IN_CACHE],
+        }
+    }
+
+    pub fn get(&self, page_num: usize) -> Option<Page> {
+        for page in self.pages.iter() {
+            if let Some(stored_page_num) = page.0 {
+                if page_num == stored_page_num {
+                    return Some(page.1)?;
+                }
+            }
+        }
+        None
     }
 }
