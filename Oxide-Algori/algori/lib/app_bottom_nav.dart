cc1c6a69cb2c320e77e8f1f8848aece63d8fbfc4
Miguevrgo <miguevrgo@gmail.com>
2024-11-22 18:42:59 +0100

    Fix theme button

--- Oxide-Algori/algori/lib/app_bottom_nav.dart
+++ Oxide-Algori/algori/lib/app_bottom_nav.dart
@@ -3,18 +3,27 @@ import 'package:flutter/material.dart';
 class AppBottomNav extends StatelessWidget {
   final int currentIndex;
   final Function(int) onItemTapped;
+  final ThemeMode currentTheme;
 
-  AppBottomNav({required this.currentIndex, required this.onItemTapped});
+  AppBottomNav(
+      {required this.currentIndex,
+      required this.onItemTapped,
+      required this.currentTheme});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: onItemTapped,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.light_mode), label: 'Theme'),
+      items: [
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.settings), label: 'Settings'),
+        const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+            icon: currentTheme == ThemeMode.light
+                ? Icon(Icons.light_mode)
+                : Icon(Icons.dark_mode),
+            label: 'Theme'),
       ],
     );
   }
