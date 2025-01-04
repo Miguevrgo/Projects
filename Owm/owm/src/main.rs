ec51d3adff62214b25ba2d1a4301edf543430119
Miguevrgo <miguevrgo@gmail.com>
2025-01-04 10:10:55 +0100

    Upload WindowManager, main, handled errors

--- Owm/owm/src/main.rs
+++ Owm/owm/src/main.rs
@@ -1,17 +1,44 @@
-use std::{io::Error, path::Display, ptr::null};
-
-use x11::xlib::{Display, Window, XDefaultRootWindow, XOpenDisplay};
+use std::error::Error;
+use std::ptr::null;
+use x11::xlib::{Display, Window, XCloseDisplay, XDefaultRootWindow, XOpenDisplay};
 
 pub struct WindowManager {
-    display: Display,
+    display: *mut Display,
     root: Window,
 }
 
 impl WindowManager {
-    fn new() -> Self {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Result<Self, Box<dyn Error>> {
         let display = unsafe { XOpenDisplay(null()) };
-        WindowManager { display, root: 8 }
+        if display.is_null() {
+            return Err(
+                "Unable to open XOpenDisplay (Maybe there is another instance running?)".into(),
+            );
+        }
+
+        let root = unsafe { XDefaultRootWindow(display) };
+        Ok(WindowManager { display, root })
+    }
+
+    pub fn close(&mut self) {
+        if !self.display.is_null() {
+            unsafe {
+                XCloseDisplay(self.display);
+            }
+        }
+    }
+}
+impl Drop for WindowManager {
+    fn drop(&mut self) {
+        self.close()
     }
 }
 
-fn main() {}
+fn main() {
+    let window_manager = WindowManager::new();
+    if let Err(e) = window_manager {
+        eprintln!("Error: {e}");
+        std::process::exit(1);
+    }
+}
