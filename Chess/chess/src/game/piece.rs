e4cdeda3ce70bdb40402a0260b38367dcab236f0
Miguevrgo <miguevrgo@gmail.com>
2025-02-15 10:04:36 +0100

    Upload get_piece

--- Chess/chess/src/game/piece.rs
+++ Chess/chess/src/game/piece.rs
@@ -12,10 +12,35 @@ pub enum Piece {
     Queen = 0b110,
 }
 
+impl Piece {
+    pub fn from(value: u8) -> Self {
+        match value {
+            0b000 => Piece::Empty,
+            0b001 => Piece::Pawn,
+            0b010 => Piece::Bishop,
+            0b011 => Piece::Knight,
+            0b100 => Piece::Rook,
+            0b101 => Piece::King,
+            0b110 => Piece::Queen,
+            _ => panic!("Invalid piece value: {}", value),
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 pub enum Color {
     White = 0b0,
     Black = 0b1,
+}
+
+impl Color {
+    pub fn from(value: u8) -> Self {
+        match value {
+            0b0 => Color::White,
+            0b1 => Color::Black,
+            _ => panic!("Invalid color: {}", value),
+        }
+    }
 }
 
 //TODO: Use this in board, considering color
