f3042530474b8d7ba27b2b661098929fd569b7f2
Miguevrgo <miguevrgo@gmail.com>
2025-02-27 09:50:38 +0100

    Upload en passant
    En passant move has been corrected to be fully functional, also, two
    bools have been added to implement the logic of whether or not a castle
    is valid

--- Chess/chess/src/game/moves.rs
+++ Chess/chess/src/game/moves.rs
@@ -1,21 +1,35 @@
-use crate::game::piece::Piece;
+use crate::game::piece::*;
 
 pub struct Move {
     piece: Piece,
-    prev_x: usize,
-    prev_y: usize,
-    new_x: usize,
-    new_y: usize,
+    prev_row: usize,
+    prev_col: usize,
+    new_row: usize,
+    new_col: usize,
 }
 
 impl Move {
-    pub fn from(piece: Piece, prev_x: usize, prev_y: usize, new_x: usize, new_y: usize) -> Self {
+    pub fn from(
+        piece: Piece,
+        prev_row: usize,
+        prev_col: usize,
+        new_row: usize,
+        new_col: usize,
+    ) -> Self {
         Move {
             piece,
-            prev_x,
-            prev_y,
-            new_x,
-            new_y,
+            prev_row,
+            prev_col,
+            new_row,
+            new_col,
         }
+    }
+
+    pub fn is_en_passant(&self) -> bool {
+        if self.piece == Piece::Pawn && self.prev_row.abs_diff(self.new_row) == 2 {
+            return true;
+        }
+
+        false
     }
 }
