5391ef9a9dd30477afa6e6b4bd208312b23ee0d3
Miguevrgo <miguevrgo@gmail.com>
2025-03-12 14:09:34 +0100

    Upload board + castle

--- Chess/chess/src/game/board.rs
+++ Chess/chess/src/game/board.rs
@@ -1,7 +1,17 @@
-use crate::game::bitboard::BitBoard;
-use crate::game::piece::Piece;
+use super::square::Square;
+use crate::game::{
+    bitboard::BitBoard,
+    castle::CastlingRights,
+    piece::{Colour, Piece},
+};
 
+#[derive(Copy, Clone, Debug)]
 pub struct Board {
-    pieces: [BitBoard; 6]; 
-}
+    pieces: [BitBoard; 6],
+    sides: [BitBoard; 2],
 
+    en_passant: Option<Square>,
+    castling_right: CastlingRights,
+    halfmoves: u8,
+    pub side: Colour,
+}
--- Chess/chess/src/game/mod.rs
+++ Chess/chess/src/game/mod.rs
@@ -1,5 +1,6 @@
 pub mod bitboard;
 pub mod board;
+pub mod castle;
 pub mod chess;
 pub mod piece;
 pub mod square;
