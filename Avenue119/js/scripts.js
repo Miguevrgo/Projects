1b44ae088c85cb7b8c3f08b54ea07d059658435e
Miguevrgo <miguevrgo@gmail.com>
2025-03-17 21:49:13 +0100

    DELETE AVENUE119 BARBER

--- Avenue119/js/scripts.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 0) {
-        nav.classList.add('scrolled');  // Añade la clase cuando se hace scroll
-    } else {
-        nav.classList.remove('scrolled');  // Elimina la clase cuando no hay scroll
-    }
-});
-
