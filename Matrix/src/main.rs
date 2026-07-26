9d38c448947bc84696a8f8efd5a3fa65547dd1dc
Miguel Angel De la Vega Rodriguez <miguel.delavega@wazuh.com>
2026-07-26 22:56:03 +0200

    feat: make it faster, refactor

--- Matrix/src/main.rs
+++ Matrix/src/main.rs
@@ -1,64 +1,63 @@
-use crossterm::cursor::{self, MoveTo};
+use crossterm::cursor::MoveTo;
 use crossterm::event::{KeyCode, poll};
-use crossterm::execute;
 use crossterm::terminal::{self, disable_raw_mode, enable_raw_mode};
 use rand::RngExt;
-use std::io::{self, Write};
+use std::io::{self, Stdout, Write};
 use std::time::Duration;
 
-const GREEN: &str = "\x1b[1;32m";
+const MAX_COLS: usize = 512;
+const GREEN: &str = "\x1b[0;38;5;46m";
+const CHARS: &[u8] = b"0123456789ABEFGJKLMNOPQRSTUVWXYZabcfgjklmnopqrstuvwxyz@#$%&*+=<>~:;{}[]|/";
 
-fn main() {
-    enable_raw_mode().unwrap();
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
     let mut stdout = io::stdout();
-    execute!(stdout, cursor::Hide).unwrap();
-    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+    write!(stdout, "\x1b[?25l\x1b[2J{GREEN}")?;
+    run_matrix_loop(&mut stdout)?;
+    write!(stdout, "\x1b[?25h\x1b[2J\x1b[1;1H\x1b[0m")?;
+    disable_raw_mode()
+}
 
-    let columns = terminal::size().unwrap().0;
-    let rows = terminal::size().unwrap().1;
-    let mut current_row = vec![0; columns as usize];
-    let mut current_row_streak = vec![0; columns as usize];
+fn run_matrix_loop(stdout: &mut Stdout) -> io::Result<()> {
+    let (cols, rows) = terminal::size()?;
+    let mut current_row = [0; MAX_COLS];
+    let mut current_row_streak = [0; MAX_COLS];
     let mut rng = rand::rng();
 
     loop {
         for _ in 0..rows {
-            for col in 0..columns as usize {
+            for col in 0..cols as usize {
                 if current_row[col] >= rows - 1 {
                     for row in 0..rows {
-                        execute!(stdout, MoveTo(col as u16, row)).unwrap();
-                        print!(" ");
+                        write!(stdout, "{} ", MoveTo(col as u16, row))?;
                     }
                     current_row[col] = 0;
                     current_row_streak[col] = 0;
                 }
 
                 if current_row[col] != 0 || rng.random_bool(0.05) {
-                    execute!(stdout, MoveTo(col as u16, current_row[col])).unwrap();
-                    if current_row_streak[col] == 0 && rng.random_bool(0.1) {
-                        current_row_streak[col] = rng.random_range(0..10);
+                    if current_row_streak[col] == 0 && rng.random_bool(0.1) || current_row[col] == 0
+                    {
+                        current_row_streak[col] = rng.random_range(0..(rows >> 2));
                     }
 
-                    let char = (rng.random_range(60..127) as u8) as char;
+                    let char = CHARS[rng.random_range(0..CHARS.len())] as char;
                     current_row[col] += 1;
 
                     if current_row_streak[col] > 0 {
-                        print!("{GREEN}{char}");
+                        write!(stdout, "{}{char}", MoveTo(col as u16, current_row[col]))?;
                         current_row_streak[col] -= 1;
                     }
                 }
             }
-            stdout.flush().unwrap();
-            std::thread::sleep(std::time::Duration::from_millis(50));
-        }
+            stdout.flush()?;
 
-        if poll(Duration::from_millis(1)).unwrap()
-            && let crossterm::event::Event::Key(k) = crossterm::event::read().unwrap()
-            && k.code == KeyCode::Char('c')
-        {
-            break;
+            if poll(Duration::from_millis(50))?
+                && let crossterm::event::Event::Key(k) = crossterm::event::read()?
+                && k.code == KeyCode::Char('c')
+            {
+                return Ok(());
+            }
         }
     }
-
-    execute!(stdout, cursor::Show).unwrap();
-    disable_raw_mode().unwrap();
 }
