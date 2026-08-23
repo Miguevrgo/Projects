37a54aafe7f85665a9c8e6d9b28b6b630cc584a8
Miguel Angel De la Vega Rodriguez <miguel.delavega@wazuh.com>
2026-08-23 23:05:16 +0200

    feat: upload some collisions handling

--- Arkanoid/main.c
+++ Arkanoid/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -8,9 +9,10 @@
 #define HEIGHT 1080
 #define BRICKS_PER_ROW 20
 #define ROWS 10
+#define MAX_SPEED 1
 
 typedef struct Player {
-    Vector2 position;
+    Vector2 pos;
     Vector2 size;
     uint8_t lifes;
 } Player;
@@ -53,14 +55,14 @@ void InitGame(GameData* const data) {
     data->player.lifes = 10;
     data->player.size.x = WIDTH * 0.1;
     data->player.size.y = HEIGHT * 0.01;
-    data->player.position.x = ((float)WIDTH / 2) - (data->player.size.x / 2);
-    data->player.position.y = HEIGHT - (HEIGHT * 0.1);
+    data->player.pos.x = ((float)WIDTH / 2) - (data->player.size.x / 2);
+    data->player.pos.y = HEIGHT - (HEIGHT * 0.1);
 
     data->ball.pos.x = (float)WIDTH / 2;
-    data->ball.pos.y = data->player.position.y - ((float)HEIGHT / 4);
+    data->ball.pos.y = data->player.pos.y - ((float)HEIGHT / 4);
     data->ball.radius = 25;
-    data->ball.speed.x = 0;
-    data->ball.speed.y = 0.5f;
+    data->ball.speed.x = -2;
+    data->ball.speed.y = 0.3F;
 }
 
 void CloseGame(GameData* const data) { free(data->bricks); }
@@ -80,25 +82,75 @@ void DrawGame(const GameData* const data) {
         }
     }
 
-    DrawRectangle((int)data->player.position.x, (int)data->player.position.y, (int)data->player.size.x,
-                  (int)data->player.size.y, BLACK);
+    DrawRectangle((int)data->player.pos.x, (int)data->player.pos.y, (int)data->player.size.x, (int)data->player.size.y,
+                  BLACK);
 
     DrawCircle((int)data->ball.pos.x, (int)data->ball.pos.y, data->ball.radius, RED);
 
     EndDrawing();
 }
 
+void ResolveCollisions(GameData* const data) {
+    // FIX: Too much speed wrong angles
+    if (CheckCollisionCircleRec(data->ball.pos, data->ball.radius,
+                                (Rectangle){
+                                    .x = data->player.pos.x,
+                                    .y = data->player.pos.y,
+                                    .width = data->player.size.x,
+                                    .height = data->player.size.y,
+                                })) {
+        const float FACTOR = fabsf(data->ball.speed.x) <= MAX_SPEED || fabsf(data->ball.speed.y) <= MAX_SPEED ? -3 : -1;
+        data->ball.speed.y *= FACTOR;
+        data->ball.speed.x = (data->ball.pos.x - data->player.pos.x) / (data->player.size.x / 2) * FACTOR;
+    }
+
+    for (size_t row = 0; row < ROWS; ++row) {
+        for (size_t col = 0; col < BRICKS_PER_ROW; ++col) {
+            Brick* brick = &data->bricks[(row * BRICKS_PER_ROW) + col];
+            if (brick->active) {
+                Rectangle rect = {.x = brick->pos.x,
+                                  .y = brick->pos.y,
+                                  .width = data->brick_size.x * 0.95F,
+                                  .height = data->brick_size.y * 0.95F};
+
+                if (CheckCollisionCircleRec(data->ball.pos, data->ball.radius, rect)) {
+                    brick->active = false;
+                    data->ball.speed.y *= -1;
+                    return;
+                }
+            }
+        }
+    }
+}
+
 void UpdateGame(GameData* const data) {
     if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_H) || IsKeyDown(KEY_A)) {
-        data->player.position.x -= data->player.position.x >= 5 ? 5 : 0;
+        data->player.pos.x -= data->player.pos.x >= 5 ? 5 : 0;
     }
 
     if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_L) || IsKeyDown(KEY_D)) {
-        data->player.position.x += data->player.position.x + data->player.size.x <= WIDTH - 5 ? 5 : 0;
+        data->player.pos.x += data->player.pos.x + data->player.size.x <= WIDTH - 5 ? 5 : 0;
     }
 
     data->ball.pos.x += data->ball.speed.x;
     data->ball.pos.y += data->ball.speed.y;
+
+    if ((int)data->ball.pos.x + data->ball.radius >= WIDTH || (int)data->ball.pos.x - data->ball.radius <= 0) {
+        data->ball.speed.x *= -1;
+    }
+    if ((int)data->ball.pos.y - data->ball.radius <= 0) {
+        data->ball.speed.y *= -1;
+    }
+
+    if ((int)data->ball.pos.y + data->ball.radius >= HEIGHT) {
+        data->ball.speed.x *= 0.5F;
+        data->ball.speed.y *= 0.5F;
+        data->ball.pos.x = (float)WIDTH / 2;
+        data->ball.pos.y = data->player.pos.y - ((float)HEIGHT / 4);
+        data->player.lifes -= 1;
+    }
+
+    ResolveCollisions(data);
 }
 
 int main(void) {
